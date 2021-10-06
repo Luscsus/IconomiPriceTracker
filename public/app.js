408a017434091b54993aa081f165e1912727d89b
Luscsus <48566270+Luscsus@users.noreply.github.com>
2021-10-06 16:01:16 +0200

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,41 @@
+const FirstPrice = 158.628614
+const FirstRealPrice = 43.30
+
+const url = "https://api.iconomi.com/v1/strategies/CAR/price"
+
+var NewPrice
+
+function CheckPrice()
+{
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        NewPrice = data.price
+        CalculatPrice()
+    })
+    .then(error => {
+        console.log(error)
+    })
+}
+
+function CalculatPrice() 
+{
+    var Change = 0
+    var ChangeInPrice = 0
+
+    if (NewPrice >= FirstPrice)
+    {
+        Change = ((NewPrice - FirstPrice) / FirstPrice) * 100
+        ChangeInPrice = (Change * FirstPrice) / 100
+        TotalPrice = ChangeInPrice + FirstRealPrice
+    }
+    else 
+    {
+        Change = ((FirstPrice - NewPrice) / FirstPrice) * 100
+        ChangeInPrice = (Change * FirstPrice) / 100
+        TotalPrice = FirstRealPrice - ChangeInPrice
+    }
+
+    document.getElementById("ProfitText").innerHTML = "Profit: " + Change.toFixed(2) + "%";
+    document.getElementById("TextTotalInvested").innerHTML = "Total invested: " + TotalPrice.toFixed(2) + "€";
+}
